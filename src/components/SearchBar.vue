8356ed1b1f2fc59a9af4c09c34454c951958d6ff
JoseDCaballero <22090601@alumno.utmetropolitana.edu.mx>
2024-07-04 22:03:58 -0600

    Mejor vista de la barra de busqueda

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -1,7 +1,10 @@
 <!-- SearchBar.vue -->
 <template>
-    <div class="search-bar">
-        <input type="text" v-model="query" @input="onInput" placeholder="Buscar...">
+    <div class="search-bar-container">
+        <div class="search-bar">
+            <input type="text" v-model="query" @input="onInput" placeholder="Buscar">
+            <i class="fas fa-search search-icon"></i>
+        </div>
     </div>
 </template>
 
@@ -24,14 +27,37 @@ watch(query, (newValue) => {
 </script>
 
 <style scoped>
-.search-bar {
+.search-bar-container {
+    display: flex;
+    justify-content: center;
     margin-bottom: 20px;
+    width: 100%;
+}
+
+.search-bar {
+    position: relative;
+    width: 100%;
+    max-width: 400px; /* Ancho máximo del input */
 }
 
 .search-bar input {
-    width: 100%;
-    padding: 10px;
+    width: calc(100% - 40px); /* Ancho del input restando el espacio para el ícono */
+    padding: 10px 20px; /* Ajuste de padding */
     border: 1px solid #ccc;
-    border-radius: 5px;
+    border-radius: 10px;
+    font-size: 16px;
+}
+
+.search-bar input:focus {
+    outline: none;
+    box-shadow: 0 0 0 2px rgba(66, 153, 225, 0.6); /* Ejemplo de estilo al enfocar */
+}
+
+.search-icon {
+    position: absolute;
+    right: 10px; /* Posición del ícono a la derecha */
+    top: 50%;
+    transform: translateY(-50%);
+    color: #666; /* Color del ícono */
 }
 </style>
